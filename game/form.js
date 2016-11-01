722d14740bb84e60f029b5ba72c8652df7b6cdd1
Jesse <skouriskaze@gmail.com>
2016-11-01 16:48:18 -0400

    Pushing form changes

---
+++ game/form.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {};
+    for (var i = 0; i < params.length; i++) {
+        var tmp = params[i].split('=');
+        data[tmp[0]] = tmp[1];
+    }
+
+    var div = document.getElementById("Name");
+    div.innerHTML = data["username"];
+}
