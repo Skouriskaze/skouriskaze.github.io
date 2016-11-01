f76b30f1b1437d1ae55c5908fb6e4d6cabbfe0b8
Jesse <skouriskaze@gmail.com>
2016-11-01 13:05:10 -0400

    Reorg

---
+++ game/resources.js
@@ -0,0 +1,20 @@
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function(callback){
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+function loadImage(paths) {
+    for (var i = 0; i < paths.length; i++) {
+        var img = new Image();
+        if (i == paths.length - 1) {
+            img.onload = gameLoop;
+        }
+        img.src = paths[i];
+    }
+}
