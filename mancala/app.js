faabf8c075cd99c684fc4253a936c5b5e1c04d3c
skouriskaze <skouriskaze@gmail.com>
2017-01-05 19:56:01 -0500

    Mancala

---
+++ mancala/app.js
@@ -0,0 +1,53 @@
+// JavaScript source code
+
+function addTab() {
+    var input = $('<input>').attr('id', 'gameName'); // Textbox to be added
+
+    // Changing tab
+    $('#newGameLabel').text("");
+    $('#addTab').attr('onclick', '');
+    $('#newGameForm').append(input);
+
+    // Autofocus textbox
+    input.focus();
+
+    // Setting input dissapear
+    input.focusout(clearAddTab);
+}
+
+function clearAddTab(cancelTab) {
+    var gameName = $('#gameName');
+    var name = gameName.val();
+
+    // Resetting tab
+    $('#addTab').attr('onclick', 'addTab()');
+    $('#newGameLabel').text("+");
+    $('#newGameForm').empty();
+}
+
+function addGame() {
+    // Tab Stuff ------------------------------------------------------------
+    var gameName = $('#gameName');
+    var name = gameName.val();
+
+    // Adding tab
+    var addItem = $('#addItem');
+    var anchor = $('<a>')
+        .attr('data-toggle', 'tab')
+        .attr('href', '#' + name)
+        .text(name);
+
+    $('<li>').append(anchor).insertBefore(addItem);
+    clearAddTab();
+
+
+
+    // Tab Content Stuff ---------------------------------------------------
+    var tabContent = $('#tabContent');
+    var tabToAdd = $('<div>').attr('id', name).addClass('tab-pane fade');
+    tabToAdd.append($('<h3>').text(name));
+
+    tabContent.append(tabToAdd);
+
+    anchor.click();
+}
