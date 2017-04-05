089ffc5bb40f2a96f567094351386097db739e13
skouriskaze <skouriskaze@gmail.com>
2017-04-05 17:23:21 -0400

    Easter

---
+++ sketch.js
@@ -0,0 +1,17 @@
+console.log("Loaded!");
+
+
+function preload() {
+}
+
+function setup() {
+    // var canvas = createCanvas(800, 400);
+    // var jqCanvas = $('#' + canvas.canvas.id);
+    // jqCanvas.appendTo('#canvas_holder');
+    //
+    
+}
+
+function draw() {
+    //background(120);
+}
